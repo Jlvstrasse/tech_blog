371045c95371f035b92c38c982e034a0997dc34d
Jlvstrasse <jlvstrasse@gmail.com>
2024-07-10 20:12:43 -0400

    config

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,12 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.tech_blog,
+  process.env.user,
+  process.env.password,
   {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    host: process.env.localhost,
+    dialect: process.env.postgres,
     port: 5432,
   }
 );
